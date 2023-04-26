006a1ad90ddfbbb9fc278e9fabd9a63057869700
C00275756 Steve Fasoranti <113533879+StevedaLegend@users.noreply.github.com>
2023-04-26 15:51:08 +0100

    Delete multiply.asm

--- integrationx32/multiply.asm
+++
@@ -1,9 +0,0 @@
-global mul
-
-section .data
-section .text
-mul:
-    mov   eax, [esp+4]      ; argument 1 offset by 4 bytes (size of interger), first 4 bytes current instruction
-    mul   dword [esp+6]      ; argument 2 offset by 4 bytes (size of interger)
-    mul   dword [esp+2]
-    ret
