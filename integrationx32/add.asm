6c53700a65b497d5cb7fa8111df3d9841eb3e531
C00275756 Steve Fasoranti <113533879+StevedaLegend@users.noreply.github.com>
2023-04-26 15:50:00 +0100

    Delete add.asm

--- integrationx32/add.asm
+++
@@ -1,9 +0,0 @@
-global add
-
-section .data
-section .text
-add:
-    mov   eax, [esp+4]      ; argument 1 offset by 4 bytes (size of interger), first 4 bytes current instruction
-    add   eax, [esp+8]      ; argument 2 offset by 4 bytes (size of interger)
-    add   eax, [esp+2]
-    ret
